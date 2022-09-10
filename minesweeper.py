8304958459697d37fed00bbe4aaefef97609de7b
PoivronJaune <poivronjaune@gmail.com>
2022-09-10 15:38:21 -0400

    Click mouse button

--- minesweeper.py
+++ minesweeper.py
@@ -1,16 +1,19 @@
 import random
 import pygame
 
+#TODO: Implement uncover the field when clicked a blank cell, upto cells that contain a value or user flag
+
 
 class Minesweeper:
     """Import Minesweeper, instance = Minesweeper(), instance.run()"""
 
     CAPTION = "Minesweeper 0.1"
     ICON = "Minesweeper.PNG"
-    WIDTH, HEIGHT = 400, 450
+    WIDTH, HEIGHT = 401, 450
 
     BG_COLOR = "white"
-    CELL_COLOR = "grey"
+    CELL_COLOR_HIDDEN = (128, 128, 128)
+    CELL_COLOR_VISIBLE = (200, 200, 200)
     CELL_BORDER = "black"
     ROWS, COLS = 15, 15
     MAX_BOMBS = 10
@@ -18,6 +21,10 @@ class Minesweeper:
     BOMB_ID = -1
     FLAG_ID = -2
     EMPTY_ID = 0
+    CELL_STATE_HIDDEN = 0
+    CELL_STATE_VISIBLE = 1
+    N_COLORS = {1:"black", 2:"blue", 3:"yellow", 4:"orange", 5:"purple", 6:"pink", 7:"cyan", 8:"magenta"}
+    CELL_SIZE = round(WIDTH / COLS, 0)
 
     def __init__(self):
         pygame.init()
@@ -49,6 +56,7 @@ class Minesweeper:
 
         pygame.quit()
 
+
     def print_mine_field_to_console(self):
         for row in range(0, Minesweeper.ROWS):
             for col in range(0, Minesweeper.COLS):
@@ -58,12 +66,26 @@ class Minesweeper:
 
     def play(self, event):
         # Add play functions here
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            row, col = self.get_grid_pos(pygame.mouse.get_pos())
+            if row < Minesweeper.ROWS or col < Minesweeper.COLS:
+              self.cover_field[row][col] = Minesweeper.CELL_STATE_VISIBLE
         self.draw()
+
+    def get_grid_pos(self, mouse_pos):
+        mx, my = mouse_pos
+        row = int(my / Minesweeper.CELL_SIZE)
+        col = int(mx / Minesweeper.CELL_SIZE)
+        return row,col
 
     def create_empty_mine_field(self, field_rows, field_cols):
         self.mine_field = [
             [Minesweeper.EMPTY_ID for _ in range(field_cols)] for _ in range(field_rows)
         ]
+        self.cover_field = [
+            [Minesweeper.CELL_STATE_HIDDEN for _ in range(field_cols)] for _ in range(field_rows)
+        ]
+
 
     def generate_bomb_positions(self, max_bombs):
         self.bomb_positions = set()
@@ -119,20 +141,29 @@ class Minesweeper:
         self.draw_mine_field_values()
         pygame.display.update()
 
+
     def draw_mine_field_values(self):
-        height_offset = 0
-        cell_size = Minesweeper.WIDTH // Minesweeper.COLS
+        cell_size = Minesweeper.CELL_SIZE
         for i, row in enumerate(self.mine_field):
             row_y = cell_size * i
             for j, field_value in enumerate(row):
                 col_x = cell_size * j
-                pygame.draw.rect(self.win, Minesweeper.CELL_COLOR, (col_x, row_y+height_offset, cell_size, cell_size))
-                pygame.draw.rect(self.win, Minesweeper.CELL_BORDER, (col_x, row_y+height_offset, cell_size, cell_size), 2)
-                text = self.CELL_FONT.render(str(field_value), 1, "red")
-                text_x = col_x + cell_size / 2 - text.get_width() / 2
-                text_y = row_y + cell_size / 2 - text.get_height() / 2
-                self.win.blit(text, (text_x, text_y))
-                
+                is_covered = self.cover_field[i][j] == 0
+                if not is_covered:
+                    pygame.draw.rect(self.win, Minesweeper.CELL_COLOR_VISIBLE, (col_x, row_y, cell_size, cell_size))
+                    pygame.draw.rect(self.win, Minesweeper.CELL_BORDER, (col_x, row_y, cell_size, cell_size), 2)
+                    if field_value > 0:
+                        text = self.CELL_FONT.render(str(field_value), 1, Minesweeper.N_COLORS[field_value])
+                        text_x = col_x + cell_size / 2 - text.get_width() / 2
+                        text_y = row_y + cell_size / 2 - text.get_height() / 2
+                        self.win.blit(text, (text_x, text_y))
+                    if field_value == -1:
+                        pygame.draw.rect(self.win, 'red', (col_x, row_y, cell_size, cell_size))
+                        pygame.draw.rect(self.win, Minesweeper.CELL_BORDER, (col_x, row_y, cell_size, cell_size), 2)                        
+                else:
+                    pygame.draw.rect(self.win, Minesweeper.CELL_COLOR_HIDDEN, (col_x, row_y, cell_size, cell_size))
+                    pygame.draw.rect(self.win, Minesweeper.CELL_BORDER, (col_x, row_y, cell_size, cell_size), 2)
+
 
 
 def main():
